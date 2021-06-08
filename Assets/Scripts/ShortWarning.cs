f00c3e004c7fbea09318e146e899bfe799e9a0d7
Wendlijl <69917501+Wendlijl@users.noreply.github.com>
2021-06-08 09:20:02 -0400

    Updating resource depleted message, dialog menu button ordering, and creating a windows icon
    Changed the fade rate on the resources depleted message so it stays on the screen longer. Also changed the text from "All resources have been collected from this planet" to "resources depleted".
    
    Updated the UI in the tutorial level so that dialog buttons are no longer on top of the pause menu.
    
    Created an icon image to use for the windows build of the game.

--- Assets/Scripts/ShortWarning.cs
+++ Assets/Scripts/ShortWarning.cs
@@ -14,7 +14,7 @@ public class ShortWarning : MonoBehaviour
     public IEnumerator FadeOut()
     {
         gameObject.GetComponent<Image>().canvasRenderer.SetAlpha(1.0f);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(3f);
         gameObject.GetComponent<Image>().CrossFadeAlpha(0.0f, 1, false);
         yield return new WaitForSeconds(1f);
         uicontroller.DisableResourceCollectedWarning();
--- Assets/Scripts/ShortWarningText.cs
+++ Assets/Scripts/ShortWarningText.cs
@@ -9,7 +9,7 @@ public class ShortWarningText : MonoBehaviour
     public IEnumerator FadeOut()
     {
         gameObject.GetComponent<TMP_Text>().canvasRenderer.SetAlpha(1.0f);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(3f);
         gameObject.GetComponent<TMP_Text>().CrossFadeAlpha(0.0f, 1, false);
         yield return new WaitForSeconds(1f);
         
